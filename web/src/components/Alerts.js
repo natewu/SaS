b8121540c2f146e90a341e73e9275f2dd2641d3e
natewu <36091727+natewu@users.noreply.github.com>
2021-06-27 02:28:20 -0600

    ui

--- web/src/components/Alerts.js
+++ web/src/components/Alerts.js
@@ -1,96 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import addNotification from 'react-push-notification';
 
 function Alerts() {
    var time = new Date();
    var date = time.toLocaleTimeString('en-GB', { hour12: true, hour: "2-digit", minute: "2-digit" });
-   const [alertList, setAlertList] = useState([
-      {
-         text: "Rifle alert!",
-         time: "1pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "2pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "3pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: "4pm"
-      },
-      {
-         text: "Pistol detected!!!",
-         time: date
-      },
-   ])
+   const [alertList, setAlertList] = useState([])
    
-   const [recentAlert, setRecentAlert] = useState([]);
-
+   const [recentAlert, setRecentAlert] = useState([null]);
+   const isFirstRun = useRef(true);
+     
    function pushAlert(alert){
-      console.log(recentAlert)
+      console.log(alertList)
       addNotification({
           title: 'Warning - ' + alert.time,
           message: alert.text,
@@ -100,19 +20,27 @@ function Alerts() {
    }
 
    useEffect(() => {
-      setRecentAlert(alertList[alertList.length - 1]);
+      if (isFirstRun.current) {
+         isFirstRun.current = false;
+         return;
+      }
+      // setRecentAlert(alertList[alertList.length - 1]);
+      // if(recentAlert[0] !== null){
+         // pushAlert(recentAlert)
+         pushAlert(alertList[alertList.length - 1]);
+      // }
    }, [alertList])
 
-   useEffect(() => {
-      if(recentAlert.length !== 0){
-         pushAlert(recentAlert);
-         console.log(recentAlert)
-      }
-   }, [recentAlert])
+   // useEffect(() => {
+   //    if(recentAlert.length !== 0){
+   //       pushAlert(recentAlert);
+   //       console.log(recentAlert)
+   //    }
+   // }, [recentAlert])
 
    return (
       <div className="alerts">
-         <h1 onClick={() => {pushAlert(recentAlert)}}>Alerts</h1>
+         <h1 >Alerts</h1>
          <div className="alert__box">
             <div className="messages">
                {alertList.map(message => 
